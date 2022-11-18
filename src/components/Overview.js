4ad2f8a8bddf2ebfdec90d2f52e17476131074c9
Kevin Browne <kbrowne97@gmail.com>
2022-11-17 21:42:10 -0500

    adds fontawesome icon

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareMinus } from "@fortawesome/free-solid-svg-icons";
 
 const Overview = ({ tasks }) => {
   return tasks.map((task, index) => {
@@ -6,6 +8,7 @@ const Overview = ({ tasks }) => {
       <div className="task" key={task.id}>
         <label>{index + 1}</label>
         <h1 key={task.id}>{task.text}</h1>
+        <FontAwesomeIcon className="delete-icon" icon={faSquareMinus} />
       </div>
     );
   });
